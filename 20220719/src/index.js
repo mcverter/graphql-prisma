170b050ef6d28ca6b196f337b82813e0a12d4b45
mcverter <mitchell.verter@gmail.com>
2022-07-19 18:41:35 -0400

    through 2-11

--- 20220719/src/index.js
+++ 20220719/src/index.js
@@ -7,6 +7,13 @@ console.log('Hello GraphQL');
 
 const typeDefs = `
   type Query {
+    greeting(name: String, position: String): String!
+    addTwo(a: Float!, b: Float!): Float!
+    add(numbers: [Float!]): Float!
+    grades: [Int!]!
+    me: User!
+    post: Post!
+
     title: String!
     price: Float!
     releaseYear: Int
@@ -17,10 +24,57 @@ const typeDefs = `
     location: String!
     bio: String!
   }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
+
+
 `
 
 const resolvers = {
     Query: {
+	greeting(parent, args, ctx, info) {
+	    if (args.name && args.position) {
+		return `hello ${args.name}. you str ${args.position}`;
+	    } else {
+		return 'hello'
+	    }
+	},
+	grades(parent, args, ctx, info) {
+	    return [10, 12, 13]
+	}
+	add(parent, args, ctx, info) {
+	    return args.numbers.reduce((acc, curr) => acc + curr, 0)
+	},
+	add2(parent, args, ctx, info){
+	    return args.a + args.b
+	},
+	me() {
+	    return {
+		id: '123',
+		name: 'mitchell',
+		email: 'mitchell@example.com'
+	    }
+	},
+	post() {
+	    return {
+		id: '092',
+		title: 'Graphql 101',
+		body: '',
+		published: false
+	    }
+	},
 	title(){
 	    "the art of war"
 	},
